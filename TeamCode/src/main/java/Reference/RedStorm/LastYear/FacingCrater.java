ed1259d5801c2e74dbfa91e51e065282b7c56d7d
skocik <skocik@us.ibm.com>
2019-10-23 16:05:00 -0400

    Added Reference package and code

---
+++ TeamCode/src/main/java/Reference/RedStorm/LastYear/FacingCrater.java
@@ -0,0 +1,132 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import RedStorm.Robot.Robot;
+
+//@Autonomous(name="FacingCrater", group="FacingCrater")
+
+@Disabled
+public class FacingCrater extends LinearOpMode {
+
+    public Robot robot = new Robot();
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        robot.initialize(hardwareMap);             // Initialize the robot
+        robot.resetEncoders();                     // Reset the encoder counts
+        robot.runWithEncoders();                   // Tell the motors to run with encoders
+
+        telemetry.addData("Status ", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        double startDeployTime = System.currentTimeMillis();
+
+        robot.setLiftServo(.6);
+
+        Thread.sleep(250);
+
+        robot.setLiftMotorPower(-.2);
+
+        while (opModeIsActive() &&
+         //       robot.getLiftEncoderCount() < 135
+         //       &&
+                System.currentTimeMillis() - startDeployTime < 3000) {
+
+        }
+        telemetry.addData("Status ", "Deployed");
+        telemetry.update();
+
+        robot.setLiftMotorPower(0);
+
+        Thread.sleep( 500);
+
+        //we should be dismounted from space lander
+
+        robot.setDriveMotorPower(.2, -.2);
+
+        while (opModeIsActive() &&
+                robot.getHeading() < 15) {
+
+        }
+        telemetry.addData("Status ", "Turned");
+        telemetry.update();
+
+        robot.resetEncoders();
+        robot.runWithEncoders();
+        robot.setDriveMotorPower(0, 0);
+
+        //we should be out of handle
+        double distanceToTravel = robot.calculateEncoderCounts(4);
+        telemetry.addData("Status ", "DistTravCalc");
+        telemetry.addData("distance to travel: ","%5.2f",distanceToTravel);
+        telemetry.addData("enc count: ","%5.2f",robot.getDriveEncoderCount());
+        telemetry.update();
+
+
+        robot.setDriveMotorPower(-.2, -.2);
+        while (opModeIsActive() &&
+                robot.getDriveEncoderCount() < distanceToTravel); {
+            telemetry.addData("Status ", "MovingAwayFromLander");
+            telemetry.addData("Enc Count:", "%5.2f",robot.getDriveEncoderCount());
+            telemetry.update();
+
+
+        }
+
+        telemetry.addData("Status ", "AwayFromLander");
+        telemetry.update();
+
+
+        robot.setDriveMotorPower(-.2, .2);
+
+        double crntHeading = robot.getHeading();
+        robot.initializeIMU();
+
+        while (opModeIsActive() &&
+                robot.getHeading() < 10) {
+            telemetry.addData("Initial heading: ", "%5.2f", crntHeading);
+            telemetry.addData("heading: ","%5.2f",robot.getHeading());
+            telemetry.update();
+        }
+        telemetry.addData("Status ", "Straight");
+        telemetry.update();
+
+        distanceToTravel = robot.calculateEncoderCounts(24);
+        robot.resetEncoders();
+        robot.runWithEncoders();
+        robot.setDriveMotorPower(-.8, -.8);
+        while (opModeIsActive() &&
+                robot.getDriveEncoderCount() < distanceToTravel) {
+        }
+        telemetry.addData("Status ", "TouchingCrater");
+        telemetry.update();
+
+        robot.setDriveMotorPower(0, 0);
+        //Turning robot to extend arm into crater
+        robot.initializeIMU();
+        while (opModeIsActive() &&
+                robot.getHeading() < 90) {
+            telemetry.addData("Initial heading: ", "%5.2f", crntHeading);
+            telemetry.addData("heading: ", "%5.2f", robot.getHeading());
+            telemetry.update();
+        }
+        robot.setDriveMotorPower(0, 0);
+        robot.setTeamMarkerArm(.1);  //Extend arm into crater
+
+
+
+
+
+
+
+        // We should now be away from handle facing the lander
+    }
+}
+
+
+
---
+++ TeamCode/src/main/java/Reference/RedStorm/LastYear/driveAlongWall.java
@@ -0,0 +1,105 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.TypeConversion;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.internal.opmode.TelemetryImpl;
+
+import RedStorm.Robot.Robot;
+
+//@Autonomous(name="Drive Along Wall", group="distance")
+@Disabled
+
+public class driveAlongWall extends LinearOpMode {
+
+
+    public Robot robot = new Robot();    // Create a new instance of the robot!
+
+
+    public void runOpMode () {
+
+        // Initialize and set up the robot's drive motors
+        robot.initialize(hardwareMap);             // Initialize the robot
+        robot.resetEncoders();                     // Reset the encoder counts
+        robot.runWithEncoders();                   // Tell the motors to run with encoders
+
+        telemetry.addData("Status:  ", "Initialized");
+        telemetry.update();
+
+        double wallDistanceToTravel = 0;
+        double distanceFromWall;
+        double wallDistanceTraveled = 0;
+        String remember = new String();
+
+
+        // Wait for the start button to be pushed!
+        waitForStart();
+
+
+            robot.resetEncoders();
+            robot.runWithEncoders();
+
+            wallDistanceToTravel = robot.calculateEncoderCounts(72);
+
+            robot.setDriveMotorPower(0.5, 0.5);
+
+            while (opModeIsActive() && wallDistanceToTravel >= wallDistanceTraveled) {
+
+                distanceFromWall = robot.getRightDistance();
+                wallDistanceTraveled = robot.getDriveEncoderCount();
+
+                telemetry.addData("left distance", "(%.2f)",robot.getLeftDistance());
+                telemetry.addData("right distance", "(%.2f)",robot.getRightDistance());
+                telemetry.addData("front distance", "(%.2f)",robot.getFrontDistance());
+                telemetry.update();
+
+                if (distanceFromWall > 5.0) {
+                    robot.setDriveMotorPower(0.5, 0.4);
+                    telemetry.addLine("turning right - towards wall");
+                    remember = "right";
+                }
+                else {
+                    if (distanceFromWall < 4.0) {
+                        robot.setDriveMotorPower(0.4, 0.5);
+                        telemetry.addLine("turning left - away from wall ");
+                        remember = "left";
+                    }
+                    else {
+                        robot.setDriveMotorPower(0.5, 0.5);
+                        telemetry.addLine("Not turning, going straight");
+                        remember = "straight";
+                    }
+                }
+
+                if (distanceFromWall > 250) {
+                        if (remember.equals("right")) {
+                            robot.setDriveMotorPower(0.4, 0.5);
+                        }
+                        else {
+                            if (remember.equals("left")) {
+                                robot.setDriveMotorPower(0.5, 0.4);
+                            }
+                            else {
+                                robot.setDriveMotorPower(0.5, 0.5);
+                            }
+                        }
+
+                }
+
+            }
+
+
+
+
+
+    }
+
+
+}
