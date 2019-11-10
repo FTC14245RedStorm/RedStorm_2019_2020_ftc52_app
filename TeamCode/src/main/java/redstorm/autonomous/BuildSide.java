65eabd3435a972c788899f2310c377da5aae1462
skocik <skocik@us.ibm.com>
2019-11-10 15:44:02 -0500

    Updated BuildSide auto code

--- TeamCode/src/main/java/redstorm/autonomous/BuildSide.java
+++ TeamCode/src/main/java/redstorm/autonomous/BuildSide.java
@@ -1,4 +1,35 @@
 package redstorm.autonomous;
 
-public class BuildSide {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import redstorm.Robot.Robot;
+
+@Autonomous(name="Stop Under Bridge", group="distance")
+
+
+public class BuildSide extends LinearOpMode {
+
+
+    public Robot robot = new Robot();    // Create a new instance of the robot
+
+
+    public void runOpMode() {
+
+        // Initialize and set up the robot's drive motors
+        robot.initialize(hardwareMap);             // Initialize the robot
+
+
+        telemetry.addData("Status:  ", "Initialized");
+        telemetry.update();
+
+        double distanceToTravel;
+
+
+        // Wait for the start button to be pushed!
+        waitForStart();
+
+    }
+
+
 }
