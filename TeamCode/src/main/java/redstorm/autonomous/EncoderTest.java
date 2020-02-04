33e02c9935964aed9f684c183be71474b5e6abde
Clyde <geyer.clyde@gmail.com>
2020-02-04 14:32:07 -0500

    fixed encoderTest

--- TeamCode/src/main/java/redstorm/autonomous/EncoderTest.java
+++ TeamCode/src/main/java/redstorm/autonomous/EncoderTest.java
@@ -15,7 +15,7 @@ import redstorm.Robot.Robot;
  * Created by Steve Kocik as a sample for RedStorm to build off of...
  */
 
-@TeleOp
+@Autonomous
 
 public class EncoderTest extends LinearOpMode {
 
@@ -40,20 +40,21 @@ public class EncoderTest extends LinearOpMode {
 
         double encCounts[] = new double[4];
 
+
         // While the autonomous period is still active AND the robot has not reached the number
         // of encoder counts to travel 24 inches
+
+        robot.setDriveMotorPower(0.5, 0.50,0.5,0.5);
+
         while(opModeIsActive()) {
             telemetry.addData("Status ", "opModeIsActive");
 
-
-            robot.setDriveMotorPower(0.5, 0.50,0.5,0.5);   // Set power to 50%
             encCounts = robot.getDriveEncoderCounts();
+
             telemetry.addData("Back Left Drive Encoder Counts","(%.0f)",encCounts[0]);
             telemetry.addData("Back Right Drive Encoder Counts","(%.0f)",encCounts[1]);
             telemetry.addData("Front Left Drive Encoder Counts","(%.0f)",encCounts[2]);
             telemetry.addData("Front Right Drive Encoder Counts","(%.0f)",encCounts[3]);
-
-            robot.setDriveMotorPower(0.0,0.0,0.0,0.0);
 
             telemetry.update();
         }
