c8a49c9b1ca70608ba59ed4d0ce0c8ae4361967d
danielhubner <danielgregg129@gmail.com>
2020-02-14 14:44:57 -0500

    autonomouns v4

--- TeamCode/src/main/java/redstorm/autonomous/BuildSideV4Blue.java
+++ TeamCode/src/main/java/redstorm/autonomous/BuildSideV4Blue.java
@@ -39,7 +39,7 @@ public class BuildSideV4Blue extends LinearOpMode {
 
         // Having the robot travel 34 inches
         snacktime.setDTMotorPosition((int)runToPosEncoderCount);
-        snacktime.setDriveMotorPower(0.75,0.1,0.75,0.1);
+        snacktime.setDriveMotorPower(0.75,0.75,0.75,0.75);
         while (opModeIsActive() && snacktime.getSortedEncoderCount() < runToPosEncoderCount) {
             telemetry.addData("Distance To Travel: ", runToPosEncoderCount);
             telemetry.addData("Encoder Count: ",snacktime.getSortedEncoderCount());
@@ -65,7 +65,7 @@ public class BuildSideV4Blue extends LinearOpMode {
 
         runToPosEncoderCount = snacktime.calculateRTPEncoderCounts(distanceToTravel);
 
-        snacktime.setDriveMotorPower(1, .75, 1, .75);
+        snacktime.setDriveMotorPower(1.0, .75, 1.0, .75);
         snacktime.setDTMotorPosition(-(int) runToPosEncoderCount);
         while (opModeIsActive() && snacktime.getSortedEncoderCount() < runToPosEncoderCount) {
             telemetry.addData("Distance To Travel: ", runToPosEncoderCount);
@@ -97,11 +97,6 @@ public class BuildSideV4Blue extends LinearOpMode {
 
         }
 
-        snacktime.setFoundationServosUp();
-        telemetry.addData("Letting Go of Foundation ", " foundation");
-        telemetry.update();
-        Thread.sleep( 500);    // Need some time to let the servos get into position
-
         // find the line and stop
 
         snacktime.resetEncoders();
@@ -109,8 +104,8 @@ public class BuildSideV4Blue extends LinearOpMode {
 
         distanceToTravel = snacktime.calculateEncoderCounts(48);
 
-        runToPosEncoderCount = snacktime.calculateRTPEncoderCounts(48.0);
-        snacktime.setDriveMotorPower(1, 1, 1, 1);
+        runToPosEncoderCount = snacktime.calculateRTPEncoderCounts(distanceToTravel);
+        snacktime.setDriveMotorPower(1.0,1.0, 1.0, 1.0);
         snacktime.setDTMotorPosition(-(int)runToPosEncoderCount);
         telemetry.addData("Hue: ", snacktime.getHue());
         telemetry.update();
