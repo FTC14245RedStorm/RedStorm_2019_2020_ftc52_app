83dedce6c31ded44c05423859de357e403973fe2
slats55 <mtvalines@gmail.com>
2020-02-19 15:26:19 -0500

    Auto Red (Working)

--- TeamCode/src/main/java/redstorm/autonomous/BuildSideV4Red.java
+++ TeamCode/src/main/java/redstorm/autonomous/BuildSideV4Red.java
@@ -84,7 +84,7 @@ public class BuildSideV4Red extends LinearOpMode {
 // //       snacktime.setFoundationServoLeft(1.0);
 //        telemetry.addData("Latching on to", " foundation");
 //        telemetry.update();
-//        Thread.sleep( 500);    // Need some time to let the servos get into position
+//        Thread.sleep( 500);    / Need some time to let the servos get into position
 
         snacktime.resetEncoders();
         snacktime.runWithEncoders();
